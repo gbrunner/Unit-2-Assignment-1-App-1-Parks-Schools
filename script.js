30a73c12e023cade0e8d63f1d51a9208c7c5e073
Gregory Brunner <gbrunner@esri.com>
2021-02-21 15:56:43 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,25 +2,25 @@ require(["esri/config","esri/Map", "esri/views/MapView", "esri/layers/FeatureLay
 
 esriConfig.apiKey =  "AAPKdc25ad7513a24c46b92cd024b53471173ysex4xHWQwBw2IQ-kNnn5MYzM2Maqnuc7YbUYeHblfF461bYrN1hiCHw4m9SK0E"; 
 
-const map = new Map({
+var map = new Map({
           basemap: "arcgis-topographic" // Basemap layer service
         });
 
-const view = new MapView({
+var view = new MapView({
           map: map,
           center: [-90.35, 38.65], // Longitude, latitude
           zoom: 11.5, // Zoom level
           container: "viewDiv" // Div element
         });
 //STL Parks feature layer (polygons) 
-const parksLayer = new FeatureLayer({
+var parksLayer = new FeatureLayer({
   url: "https://services2.arcgis.com/bB9Y1bGKerz1PTl5/arcgis/rest/services/STL_Parks/FeatureServer"
 });
   
   map.add(parksLayer);
           
 //STL Public Schools (points) 
-const pubschoolLayer = new FeatureLayer({
+var pubschoolLayer = new FeatureLayer({
   url: "https://services2.arcgis.com/bB9Y1bGKerz1PTl5/arcgis/rest/services/Buffer_of_STL_Public_Schools/FeatureServer"
 });
   
